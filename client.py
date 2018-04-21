cf7bb6eb4dd4b9d535900ad8d253eb52a2f064c8
terryliu1995 <zliu48@ncsu.edu>
2018-04-20 21:44:02 -0400

    finish part of client except download

--- client.py
+++ client.py
@@ -24,6 +24,7 @@ class Client(object):
 
     def main(self):
         print 'You has connected to server %s, address is %s \n' % (self.SERVER_HOSTNAME, self.SERVER_ADDRESS)
+        #open a thread for upload server
         upload_thread = threading.Thread(target=waitUploadConnection, args=())
         upload_thread.start()
         #wait upload listener to be prepared
@@ -32,7 +33,124 @@ class Client(object):
         self.handleRequest()
     
     def handleRequest(self):
+        print 'connect: connect to server\n'+
+              'add: add the rfc of this client\n'+
+              'query: get all active clients\n'+
+              'lookup: lookup a specific rfc\n'+
+              'list: list all rfcs\n'+
+              'download: download specific rfc\n'
+        command_dict = {'connect':self.connect_server(), 
+                        'add':self.add_rfc(),
+                        'query':self.query_active(),
+                        'lookup':self.lookup_rfc(),
+                        'list':self.list_all(),
+                        'download':self.download(),
+                        'quit':self.quit()}
+        while True:
+            request_method = raw_input('Enter your request: ')
+            command_dict.setdefault(request_method, self.invalid_request())
+
+    def connect_server(self):
+        if self.serverSocket:
+            print 'Already connected'
+        else:
+            #create socket
+            self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            try:
+                self.serverSocket.connect((self.SERVER_HOSTNAME, self.SERVER_PORT_NUM))
+            except BaseException, exc:
+                print "Caught exception: %s" % exc
+            print 'Server is connected!'
+            data = 'CONNECT %s P2P-CI/1.0\n' % (str(self.UPLOAD_PORT_NUM))
+            self.serverSocket.sendall(data)      
+
+    def add_rfc(self):
+        # get info of rfc
+        if self.serverSocket:
+            rfc_num = raw_input("Enter RFC number: ")
+            rfc_title = raw_input("Enter RFC title: ")
+            # construct data
+            data = 'ADD RFC %s P2P-CI/1.0\n' % (rfc_num)+
+                'Host: %s\n' % (socket.gethostname())+
+                'Port: %s\n' % (self.UPLOAD_PORT_NUM)+
+                'Title: %s\n' % (rfc_title)
+            self.serverSocket.sendall(data)
+            # receive response
+            recv_msg = self.serverSocket.recv(1024)
+            print recv_msg
+        else:
+            print 'please connect first'
+    
+    def query_active(self):
+        if self.serverSocket:
+            data = 'QUERY\n'
+            self.serverSocket.sendall(data)
+            recv_data = pickle.loads(self.serverSocket.recv(1024))
+            print recv_data
+        else:
+            print 'please connect first'
+
+    def list_all(self):
+        if self.serverSocket:
+            data = 'LIST ALL P2P-CI/1.0\n'+
+                   'Host: %s\n' % (socket.gethostname())+
+                   'Port: %s\n' % (self.UPLOAD_PORT_NUM)
+            self.serverSocket.sendall(data)
+            # receive response
+            recv_msg = self.serverSocket.recv(1024)
+            print recv_msg
+        else:
+            print 'please connect first'
         
+
+    def lookup_rfc(self):
+        if self.serverSocket:
+            rfc_num = raw_input("Provide RFC number: ")
+            rfc_title = raw_input("Provide RFC title: ")
+            # format request data
+            data = 'LOOKUP RFC %s P2P-CI/1.0\n' % (rfc_num)+
+                'Host: %s\n' % (socket.gethostname())+
+                'Port: %s\n' % (self.UPLOAD_PORT_NUM)+
+                'Title: %s\n' % (rfc_title)
+            self.serverSocket.sendall(data)
+            # receive response
+            recv_msg = self.serverSocket.recv(1024)
+            print recv_msg
+        else:
+            print 'please connect first'
+
+
+    def download(self):
+        if self.serverSocket:
+            rfc_num = raw_input("Enter RFC number you wish to download: ")
+            host_name = raw_input("Enter hostname from which you wish to download: ")
+            peer_upload_port = raw_input("Enter the upload port: ")
+            os_info = platform.platform()
+            rfc_path = 'rfc%s.txt' % (rfc_num)
+            data = 'GET RFC %s P2P-CI/1.0\n' % (rfc_num)+
+                   'Host: %s\n' % (host_name)+
+                   'OS: %s\n' % (os_info)
+            self.downloadSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            try:
+                self.downloadSocket.connect((host_name, int(peer_upload_port)))
+            except BaseException, exc:
+                print "Caught exception: %s" % exc
+                return
+            self.downloadSocket.sendall(data)
+            print 'requesting...'
+            recv_data = self.downloadSocket.recv(1024)
+            print recv_data
+            
+        else:
+            print 'please connect first'
+
+    def quit(self):
+        if self.serverSocket:
+            
+        else:
+            
+    def invalid_request(self):
+        print 'Error. Invalid Command!'
 
     def waitUploadConnection(self):
         self.uploadSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -51,7 +169,7 @@ class Client(object):
         print 'upload server has been closed'
 
     def buildConnection(self, connection, address):
-
+        
 
 
 
